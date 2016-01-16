60265fd492690505ff6780410dba3a265a4f59e2
ggallone <giuseppegallone@yahoo.it>
2016-01-16 15:05:25 +0000

    test commit from fgu

--- wc_compute.r
+++ wc_compute.r
@@ -1,4 +1,6 @@
 library(gplots)
+
+#test
 vdr <- read.table("/net/isi-scratch/giuseppe/VDR/RAW/34_SVR11_hg19_LOW_QUALITY/02_BAM_BOWTIE/d_correlation_all_q0.01/d_loglr/wc_R.dat",header=TRUE, row.names=1)
 pdf("/net/isi-scratch/giuseppe/VDR/RAW/34_SVR11_hg19_LOW_QUALITY/02_BAM_BOWTIE/d_correlation_all_q0.01/d_loglr/heatmap.pdf", height=10, width=10)
 heatmap.2(as.matrix(vdr),trace="none")
