18538c515a59ca227936afb4ce0cdaad093ad921
ggstatgen <giuseppegallone@yahoo.it>
2016-01-24 19:08:33 +0000

    added support for lifted over file to retrieve ensids not in gencode

--- P_Various/do_REVIEW_get_fg_bg_genes_from_gencode.pl
+++ P_Various/do_REVIEW_get_fg_bg_genes_from_gencode.pl
@@ -29,6 +29,7 @@ use IO::Zlib;
 
 my $input_gencode;
 my $input_list;
+my $lifted_over_in;
 
 my $VCFSORT = `which vcf-sort`; chomp $VCFSORT;
 my $GZIP = `which gzip`; chomp $GZIP;
@@ -36,10 +37,12 @@ my $GZIP = `which gzip`; chomp $GZIP;
 GetOptions(
         'i=s'      =>\$input_list,
 		'g=s'      =>\$input_gencode,
+		'l=s'      =>\$lifted_over_in
 ); 
-my $USAGE = "USAGE: do_REVIEW_get_fg_bg_genes_from_gencode.pl -i=<INFILE_LIST> -g=<GENCODE_PC_GENES>\n" .
+my $USAGE = "USAGE: do_REVIEW_get_fg_bg_genes_from_gencode.pl -i=<INFILE_LIST> -g=<GENCODE_PC_GENES> (opt)-l=<LIFTED_OVER_BED>\n" .
 			"<INFILE_LIST> list of foreground ENSID to convert extracted from VITD Chipseq supplementary\n" .
-			"<GENCODE_PC_GENES> gtf gz file with GENCODE v19 entries of the type 'gene' and 'protein_coding'\n";
+			"<GENCODE_PC_GENES> gtf gz file with GENCODE v19 entries of the type 'gene' and 'protein_coding'\n" . 
+			"(optional)<LIFTED_OVER_BED> hg19 bed file lifted over from the hg18 bed - used to get coords if ENS ID not found in gencode.\n";
 			
 unless($input_list && $input_gencode){
 	print STDERR $USAGE, "\n";
@@ -67,6 +70,23 @@ my $output_not_found = $directory. $basename . '_unmapped_to_' . $gen_basename .
 #.
 #gene_id "ENSG00000186092.4"; transcript_id "ENSG00000186092.4"; gene_type "protein_coding"; gene_status "KNOWN"; gene_name "OR4F5"; transcript_type "protein_coding"; transcript_status "KNOWN"; transcript_name "OR4F5"; level 2; havana_gene "OTTHUMG00000001094.1";
 
+my %lifted_over_map;
+if($lifted_over_in){
+	open (my $instream,  q{<}, $lifted_over_in) or die("Unable to open $lifted_over_in : $!");
+	while(<$instream>){
+		chomp;
+		my @fields = split("\t", $_);
+		my ($gene_name, $ens_id) = split("-", $fields[3]);
+		$lifted_over_map{$ens_id}{CHR} = $fields[0];
+		$lifted_over_map{$ens_id}{START}= $fields[1];
+		$lifted_over_map{$ens_id}{STOP}= $fields[2];
+		$lifted_over_map{$ens_id}{GENE}= $gene_name;	
+	}
+	close $instream;
+}
+
+
+
 my %fgdata;
 open (my $instream,  q{<}, $input_list) or die("Unable to open $input_list : $!");
 open (my $outstream,  q{>}, $output_not_found) or die("Unable to open $output_not_found : $!");
@@ -77,9 +97,26 @@ while(<$instream>){
 	my $line = `$grep_search`;
 	
     if(!$line || $line eq '' ){
-    	print STDERR "Warning: grepping $ens_id against the GENCODE data found nothing. Skipping this.\n";
-	print $outstream $ens_id, "\n";
-    	next;
+    	print STDERR "Warning: grepping $ens_id against the GENCODE data found nothing:";
+    	if($lifted_over_in){
+    		if($lifted_over_map{$ens_id}){
+    			print STDERR "Found $ens_id in lifted over file..\n";
+    			my $bed_line = $lifted_over_map{$ens_id}{CHR}    . "\t" .
+    							$lifted_over_map{$ens_id}{START} . "\t" .
+    							$lifted_over_map{$ens_id}{STOP}  . "\t" .
+    							'orig_coords_' . $ens_id . '-' . $lifted_over_map{$ens_id}{GENE};
+    			$fgdata{$bed_line} = 1;
+    			next;
+    		}else{
+    			print STDERR "Not found $ens_id in lifted over file..\n";
+    			print $outstream $ens_id, "\n";
+    			next;
+    		}
+    	}else{
+    		print STDERR "Saving $ens_id in $output_not_found\n";
+    		print $outstream $ens_id, "\n";
+    		next;
+    	}
     }
     my @lines = split("\n", $line);
     if(scalar @lines > 1){
@@ -88,16 +125,13 @@ while(<$instream>){
     	exit -1;
     }
     my @fields = split("\t", $lines[0]);
+    next unless($fields[3]);
+	next unless($fields[4]);
  	if($fields[2] ne 'gene'){
 		print STDERR "Warning: this line does not contain a gene: $lines[0]. Skipping..\n";
 		next;
 	}
-	next unless($fields[3]);
-	next unless($fields[4]);
-	#unless($fields[8] =~ /protein_coding/){
-	#	print STDERR "Warning: this line does not contain a protein coding gene: $lines[0]. Skipping..\n";
-	#	next;
-	#}  
+
 	my @inner_fields = split(';', $fields[8]);
 	my $bed_name = $ens_id . ';' . $inner_fields[4];
 	#ADJUST COORDS 
@@ -128,10 +162,7 @@ while (<FILE>){
 	}
 	next unless($fields[3]);
 	next unless($fields[4]);
-	#unless($fields[8] =~ /protein_coding/){
-#		print STDERR "Warning: this line does not contain a protein coding gene: $_. Skipping..\n";
-#		next;
-#	}	
+
 	my @inner_fields = split(';', $fields[8]);
 	my $bed_name = $inner_fields[0] . '; ' . $inner_fields[4];
 	my $bed_line = $fields[0] . "\t" . $fields[3] . "\t" . $fields[4] . "\t" . $bed_name; 
@@ -143,40 +174,5 @@ close FILE;
 open ($outstream,  q{>}, $output_bg) or die("Unable to open $output_bg : $!");
 foreach my $item (keys %bgdata){ print $outstream $item, "\n"; }
 close $outstream;
-
-
-#my %ensid_to_gene;
-#tie *FILE,   'IO::Zlib', $input_gencode, "rb";
-#while (<FILE>){
-#	chomp;
-#	next if($_ eq '');
-#	next if($_ =~ /^\#/);
-#	next if($_ =~ /^\#/);
-#	
-#	my @fields = split("\t", $_);
-#	#general check
-#	if($fields[2] ne 'gene'){
-#		print STDERR "Warning: this line does not contain a gene: $_. Skipping..\n";
-#		next;
-#	}
-#	next unless($fields[3]);
-#	next unless($fields[4]);
-#	unless($fields[8] =~ /protein_coding/){
-#		print STDERR "Warning: this line does not contain a protein coding gene: $_. Skipping..\n";
-#		next;
-#	}
-#	#get ensembl id
-#	my @inner_fields = split(';', $fields[8]);
-#	my $this_ens_id;
-#	#looks like this: gene_id "ENSG00000273291.1"
-#	if($inner_fields[0] =~ /gene_id\s\"(.*)\.\d+\"/){
-#		$this_ens_id = $1;
-#	}else{
-#		print STDERR "Warning: unable to extract and ensembl id from this line: $_. Aborting..\n";
-#		exit -1;
-#	}
-#		
-#}
-#close FILE;
 
 
