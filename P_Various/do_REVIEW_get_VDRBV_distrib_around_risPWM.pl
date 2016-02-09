62d345bc447ec9b65058382b78338429b4366ff4
ggstatgen <giuseppegallone@yahoo.it>
2016-02-09 22:23:23 +0000

    Polished argument interface
    -VDR-BV/VDR-rBV bed now hardcoded
    -PWM ENCODE file now hardcoded
    -PLOT TYPE now an argument

--- P_Various/do_REVIEW_get_VDRBV_distrib_around_risPWM.pl
+++ P_Various/do_REVIEW_get_VDRBV_distrib_around_risPWM.pl
@@ -3,69 +3,78 @@ use strict;
 use warnings;
 use File::Basename;
 use Getopt::Long;
-use List::Util qw( min max );
-use Statistics::R;
-
 
 #07/2/2016
 #I want to build a histogram showing the distribution of variant in the VDR:RXR motif and around it.
 #I can use bedtools closest to assign every variant to its closest VDR:RXR motif and then spit the R file
 #The problems with the interval in the ris are solved similarly to what I did in 'do_funseq_adapt_motiffile.pl'
 
-
 #use the following
 #bedtools closest -d -a Output_noDBRECUR.bed -b /net/isi-scratch/giuseppe/VDR/POOLED_4/07_BAM_STAMPY_MIN21_RBL/d_CONSENSUS_PEAKSET_GEM_MACS/d_motif_scan_pscanchip/Pscanchip_occurrences_VDRRXR_jaspar_all_motifintervals.bed > closest.bed
 #bedtools closest -D "ref" if you want it symmetrical
 
-my $THRS_DIST;
+
 my $BEDTOOLS = `which bedtools`; chomp $BEDTOOLS;
 my $RSCRIPT = `which RRscript`; chomp $RSCRIPT;
-my $CHROMSIZES = '/net/isi-scratch/giuseppe/indexes/chrominfo/hg19.chrom_simple.sizes'; 
 
-my $PLOT_EXT = 'pdf';
+my $CHROMSIZES = '/net/isi-scratch/giuseppe/indexes/chrominfo/hg19.chrom_simple.sizes'; 
+my $IN_VDRBV_BED = '/net/isi-scratch/giuseppe/VDR/ALLELESEQ/funseq2/out_allsamples_plus_qtl_ancestral/PSCANCHIP_motifs/Output_noDBRECUR.bed';
+my $IN_VDRrBV_BED = '/net/isi-scratch/giuseppe/VDR/ALLELESEQ/funseq2/out_allsamples_plus_qtl_ancestral/PSCANCHIP_motifs/Output_VDR_rBVs_hg19.bed';
+my $PWM_FILE = "/net/isi-scratch/giuseppe/VDR/ALLELESEQ/funseq2/out_allsamples_plus_qtl_ancestral/PSCANCHIP_motifs/Processed_PFMs_jaspar_FUNSEQ_INPUT.txt";
 
 my $input_pscanchip_ris;
-my $input_vdr_bv;
-my $PWM_FILE;
+my $input_variants;
+my $INPUT_VAR_BINARY;
 my $identifier;
 my $motif_name;
-my $MIN_SCORE;
-my %variant_binning;
 my $THRS_DIST;
+my $PLOT_EXT;
+my $MIN_SCORE;
 
 GetOptions(
         'i_m=s'      =>\$input_pscanchip_ris,        
-		'i_v'       =>\$input_vdr_bv,
-        'pwm=s'		=>\$PWM_FILE,	
+		'v=s'       =>\$INPUT_VAR_BINARY,	
+        'm=s'		=>\$motif_name,		
         'id=s'		=>\$identifier,
-        'm=s'		=>\$motif_name,
-        's=f'		=>\$MIN_SCORE,
-        't=i'       =>\$THRS_DIST       
+        't=i'       =>\$THRS_DIST,   
+        'p=s'       =>\$PLOT_EXT,
+        's=f'		=>\$MIN_SCORE            
 );
-$input_vdr_bv = "/net/isi-scratch/giuseppe/VDR/ALLELESEQ/funseq2/out_allsamples_plus_qtl_ancestral/PSCANCHIP_motifs/Output_noDBRECUR.bed";
-$input_pscanchip_ris = "/net/isi-scratch/giuseppe/VDR/ALLELESEQ/funseq2/out_allsamples_plus_qtl_ancestral/PSCANCHIP_motifs/RIS_LINKS/Pscanchip_hg19_bkgGM12865_Jaspar_VDRBVs_RXRA-VDR_MA0074.1_sites.ris";
-$PWM_FILE = "/net/isi-scratch/giuseppe/VDR/ALLELESEQ/funseq2/out_allsamples_plus_qtl_ancestral/PSCANCHIP_motifs/Processed_PFMs_jaspar_FUNSEQ_INPUT.txt";
-$motif_name = "RXRA-VDR";
-$identifier = "MA0074.1";
-$MIN_SCORE = '0.8';
-$THRS_DIST = 100;
+#$input_pscanchip_ris = "/net/isi-scratch/giuseppe/VDR/ALLELESEQ/funseq2/out_allsamples_plus_qtl_ancestral/PSCANCHIP_motifs/RIS_LINKS/Pscanchip_hg19_bkgGM12865_Jaspar_VDRBVs_RXRA-VDR_MA0074.1_sites.ris";
+#$motif_name = "RXRA-VDR";
+#$identifier = "MA0074.1";
+#$MIN_SCORE = '0.8';
+#$THRS_DIST = 100;
 
-
-my $USAGE = "\nUSAGE: $0 -i_m=<INFILE_PSCANCHIP> -i_v=<INFILE_VDRBV> -pwm=<ENCODE_PWM_FILE> -id=<ID> -m=<MOTIF_NAME> -t=<THRS> (opt)-s=<MINSCORE>\n" .
+my $USAGE = "\nUSAGE: $0 -i_m=<INFILE_PSCANCHIP> -i_v=<BV|rBV> -m=<MOTIF_NAME> -id=<ID> -t=<THRS> -p=<pdf|svg|jpg|png> (opt)-s=<MINSCORE>\n" .
 			"<INFILE_PSCANCHIP> ris file from PscanChip\n" .
-			"<INFILE_VDRBV> bed file of VDR-BVs\n" . 
-			"<ENCODE_PWM_FILE> text file with Jaspar PWMs in ENCODE format obtained with RSAT\n" .
+			"<BV|rBV> if BV, all VDRBV will be used; if rBV, only VDR-rBV will be used\n" .
+			"<MOTIF_NAME> string to use for the Motif PWM name in the output file\n" .			 
 			"<ID> string to use for the ID (e.g. Jaspar ID) of the PWM in the output file\n" . 
-			"<MOTIF_NAME> string to use for the Motif PWM name in the output file\n" .
 			"<THRS> Distance threshold cutoff for plot\n" .
+			"<PLOT> type of R output plot desired\n" .
 			"optional <MINSCORE> lower threshold on score (eg 0.8) (default:none)\n"; 
 			
-unless($input_pscanchip_ris && $input_vdr_bv && $identifier && $motif_name && $PWM_FILE && $THRS_DIST){
+unless($input_pscanchip_ris && $INPUT_VAR_BINARY && $identifier && $motif_name && $THRS_DIST && $PLOT_EXT){
 	print $USAGE;
+	exit -1;
+}
+#binary arguments
+if($INPUT_VAR_BINARY eq 'BV'){
+	$input_variants = $IN_VDRBV_BED;
+}elsif($INPUT_VAR_BINARY eq 'rBV'){
+	$input_variants = $IN_VDRrBV_BED;
+}else{
+	print STDERR "ERROR: field -v not recognised: $INPUT_VAR_BINARY. Aborting.\n";
+	exit -1;
+}
+unless($PLOT_EXT eq 'jpg' || $PLOT_EXT eq 'png' || $PLOT_EXT eq 'svg' || $PLOT_EXT eq 'pdf'){
+	print STDERR "ERROR: field -p not recognised: $PLOT_EXT. Aborting.\n";
 	exit -1;
 }
 print STDERR "THRESHOLDING ON SCORE: $MIN_SCORE\n" if($MIN_SCORE);
 my $this_motif_id = $motif_name . '_' . $identifier;
+
 
 my($basename, $directory) = fileparse($input_pscanchip_ris);
 $basename =~ s/(.*)\..*/$1/;
@@ -75,12 +84,13 @@ my $data_closest            = $directory . $basename . '_' . $this_motif_id  . '
 my $data_counts             = $directory . 'R_' . $this_motif_id  . '_counts.Rdata';
 my $data_histogram          = $directory . 'R_' . $this_motif_id  . '_histogram.Rdata';
 
-my $Rscript_counts           = $directory . 'R_' . $this_motif_id  . '_counts.R';
-my $Rscript_counts_plot_all  = $directory . 'R_' . $this_motif_id  . '_counts_all.' . $PLOT_EXT;
-my $Rscript_counts_plot_sub  = $directory . 'R_' . $this_motif_id  . '_counts_dthrs_' . $THRS_DIST .  '.' . $PLOT_EXT;
+my $Rscript_counts           = $directory . 'R_' .                     $this_motif_id  . '_counts.R';
 my $Rscript_hist             = $directory . 'R_' . $this_motif_id  . '_hist.R';
-my $Rscript_hist_plot_all    = $directory . 'R_' . $this_motif_id  . '_hist_all.' . $PLOT_EXT;
-my $Rscript_hist_plot_sub    = $directory . 'R_' . $this_motif_id  . '_hist_dthrs_' . $THRS_DIST .  '.' . $PLOT_EXT;
+
+my $Rscript_counts_plot_all  = $directory . 'R_' . $INPUT_VAR_BINARY . '_' . $this_motif_id . '_counts_all.'                       . $PLOT_EXT;
+my $Rscript_counts_plot_sub  = $directory . 'R_' . $INPUT_VAR_BINARY . '_' . $this_motif_id . '_counts_dthrs_' . $THRS_DIST .  '.' . $PLOT_EXT;
+my $Rscript_hist_plot_all    = $directory . 'R_' . $INPUT_VAR_BINARY . '_' . $this_motif_id . '_hist_all.'                         . $PLOT_EXT;
+my $Rscript_hist_plot_sub    = $directory . 'R_' . $INPUT_VAR_BINARY . '_' . $this_motif_id . '_hist_dthrs_'   . $THRS_DIST .  '.' . $PLOT_EXT;
 
 #######
 #1 get the REAL motif length from the encode representations of the motif
@@ -149,7 +159,7 @@ close $outstream;
 #sort bed
 system "cat $temp_pwm_bed | sort -k1,1V -k2,2n | uniq > $temp_pwm_bed_sorted";
 #get closeness data
-system "$BEDTOOLS closest -D \"ref\" -a $input_vdr_bv -b $temp_pwm_bed_sorted -g $CHROMSIZES > $data_closest";
+system "$BEDTOOLS closest -D \"ref\" -a $input_variants -b $temp_pwm_bed_sorted -g $CHROMSIZES > $data_closest";
 #from here I want two files
 #one, pure counts, to build histograms in R
 #two, x,y pairs, to build point plots in R
@@ -177,7 +187,6 @@ close $outstream;
 
 system "$RSCRIPT $Rscript_counts";
 
-
 open ($outstream,  q{>}, $Rscript_hist) or die("Unable to open $Rscript_hist : $!");
 print $outstream "data <- read.table(\"$data_histogram\",sep=\"\\t\")" . "\n";
 print $outstream "$PLOT_EXT(file=\"$Rscript_hist_plot_all\")" . "\n";
@@ -193,6 +202,10 @@ system "$RSCRIPT $Rscript_hist";
 
 unlink $data_counts;
 unlink $data_histogram;
+unlink $Rscript_counts;
+unlink $Rscript_hist;
+
+
 
 
 #bin_variants($data_histogram, \%variant_binning);
@@ -200,45 +213,39 @@ unlink $data_histogram;
 #foreach my $bin (sort {$a<=>$b} keys %variant_binning){
 #	print $bin, "\t", $variant_binning{$bin}, "\n";
 #}
-
-#R processing, including thresholding
-
-
-
-############### binning ############
-sub bin_variants{
-	my $BIN_NUMBER; #temp
-	my ($file, $hash) = @_;
-	my %local_hash; #for the background the vcf is not unique, so fill hash. Do it also for the foreground, it won't hurt
-
-# output from uniq -c
-#      1 -6685283$
-#      1 -6639883$
-#      1 -6632412$
-#      1 -6621090$
-#      1 -6504254$
-	#you need to find the maximum distance in absolute value
-	my @distances_seen;
-	open (my $instream,  q{<}, $file) or die("Unable to open $file : $!");
-	while (<$instream>)	{
-		chomp;
-		my ($number, $distance) = (split /\t/)[0,1];
-		$distance =~ s/-(\d+)/$1/;
-		push(@distances_seen, $distance);
-	}
-	close $instream;
-	my $max = max @distances_seen;
-	#print "Max distance in absolute value: $max\n";
-	
-	open ($instream,  q{<}, $file) or die("Unable to open $file : $!");
-	while (<$instream>)	{
-		chomp;
-		my ($number, $distance) = (split /\t/)[0,1];
-		my $frac_distance = $distance / $max;
-		my $bin_id = int( $frac_distance * $BIN_NUMBER )/ $BIN_NUMBER;
-		$$hash{$bin_id} += $number;
-	}	
-	close $instream;	
-	return 1;
-}
-			
+################ binning ############
+#sub bin_variants{
+#	my $BIN_NUMBER; #temp
+#	my ($file, $hash) = @_;
+#	my %local_hash; #for the background the vcf is not unique, so fill hash. Do it also for the foreground, it won't hurt
+#
+## output from uniq -c
+##      1 -6685283$
+##      1 -6639883$
+##      1 -6632412$
+##      1 -6621090$
+##      1 -6504254$
+#	#you need to find the maximum distance in absolute value
+#	my @distances_seen;
+#	open (my $instream,  q{<}, $file) or die("Unable to open $file : $!");
+#	while (<$instream>)	{
+#		chomp;
+#		my ($number, $distance) = (split /\t/)[0,1];
+#		$distance =~ s/-(\d+)/$1/;
+#		push(@distances_seen, $distance);
+#	}
+#	close $instream;
+#	my $max = max @distances_seen;
+#	#print "Max distance in absolute value: $max\n";
+#	
+#	open ($instream,  q{<}, $file) or die("Unable to open $file : $!");
+#	while (<$instream>)	{
+#		chomp;
+#		my ($number, $distance) = (split /\t/)[0,1];
+#		my $frac_distance = $distance / $max;
+#		my $bin_id = int( $frac_distance * $BIN_NUMBER )/ $BIN_NUMBER;
+#		$$hash{$bin_id} += $number;
+#	}	
+#	close $instream;	
+#	return 1;
+#}
